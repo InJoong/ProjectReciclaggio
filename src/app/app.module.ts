b327d4431f4e3d9392d36aa30cbb5280b72f216d
Victor Lopez <A01336434@itesm.mx>
2020-04-27 19:21:30 -0500

    Added layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutHeaderComponent } from './main-components/layout-header/layout-header.component';
+import { LayoutFooterComponent } from './main-components/layout-footer/layout-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutHeaderComponent,
+    LayoutFooterComponent
   ],
   imports: [
     BrowserModule
