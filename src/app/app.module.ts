eacea58e8dc8cb28019b42763139969712239be2
InJoong <injoong01@hotmail.com>
2020-04-27 20:15:16 -0500

    Added User and Driver view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DriverHomeModule } from './modules/driver-home/driver-home.module';
+import { UserHomeModule } from './modules/user-home/user-home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DriverHomeModule,
+    UserHomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
