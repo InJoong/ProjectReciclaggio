e2af0481a863eca1312c26dc2bb27f089f1ee12a
Victor Lopez <A01336434@itesm.mx>
2020-05-10 20:58:17 -0500

    Made form behave according to ngForm

---
+++ src/app/data/subcategories.ts
@@ -0,0 +1,22 @@
+export const SUBCATEGORIES = [
+  {
+    id: 1,
+    categoria_id: 1,
+    nombre: 'Otros'
+  },
+  {
+    id: 2,
+    categoria_id: 2,
+    nombre: 'Mesas'
+  },
+  {
+    id: 3,
+    categoria_id: 3,
+    nombre: 'Laptops'
+  },
+  {
+    id: 4,
+    categoria_id: 4,
+    nombre: 'Cascajo grande'
+  }
+];
--- src/app/modules/user/components/first-step/first-step.component.ts
+++ src/app/modules/user/components/first-step/first-step.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CATEGORIES} from '../../../../data/categories';
+import {SUBCATEGORIES} from '../../../../data/subcategories';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-first-step',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-step.component.scss']
 })
 export class FirstStepComponent implements OnInit {
+  categories = CATEGORIES;
+  subcategories = SUBCATEGORIES;
 
-  constructor() { }
+  selectedCategory = 1;
+  constructor(private router: Router) { }
+
+  changeCategory(newValue) {
+    this.selectedCategory = newValue;
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+    localStorage.setItem('categoriaForm', f.value.categoria);
+    localStorage.setItem('subcategoriaForm', f.value.subcategoria);
+    localStorage.setItem('altoForm', f.value.alto);
+    localStorage.setItem('pesoForm', f.value.peso);
+    localStorage.setItem('anchoForm', f.value.ancho);
+    localStorage.setItem('largoForm', f.value.largo);
+    this.router.navigateByUrl('/user/nueva-orden/2');
   }
 
 }
