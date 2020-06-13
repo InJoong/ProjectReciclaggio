e1fa994f43a1843846522ab97df92e1a6e324189
InJoong <injoong01@hotmail.com>
2020-06-13 04:17:02 -0500

    user and driver card filtering, user registration on the DB

--- src/app/modules/driver/components/driver-layout/driver-layout.component.ts
+++ src/app/modules/driver/components/driver-layout/driver-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { AuthDriverService } from 'src/app/services/auth-driver.service';
 
 @Component({
   selector: 'app-driver-layout',
@@ -10,14 +11,14 @@ export class DriverLayoutComponent implements OnInit {
 
   orders;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private authdriver: AuthDriverService) { }
 
   ngOnInit(): void {
     this.getOrders();
   }
 
   getOrders() {
-    this.api.getOrders$().subscribe(
+    this.api.getOrders$("/driver/"+this.authdriver.userProfile$.source['_value'].sub).subscribe(
       res => this.orders = res
     );
   }
